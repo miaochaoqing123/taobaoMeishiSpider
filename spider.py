c3f355b0cddbb9bf494db91f3edcd33d2e2a84e9
缪超青 <419666193@qq.com>
2018-04-29 09:22:27 +0800

    '解析网页内容,分析出想要的数据'

--- spider.py
+++ spider.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pyquery import PyQuery as pq
 
 driver = webdriver.Chrome()
 # headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36"}
@@ -25,13 +26,9 @@ def search():
         # sleep(3)
         # 取出总页数
         total = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager > div > div > div > div.total')))
-        # print(total)
+        get_products()  # 调用下面的解析方法
         total = total.text
-        # 正则匹配
         total = int(re.compile('(\d+)').search(total).group(1))
-        # print(total)
-        # sleep(3)
-        # driver.quit()
         return total
     except TimeoutError:
         return search()
@@ -51,9 +48,28 @@ def next_page(page_number):
         submit_tb.click()  # 点击确认,转入到下一页
         # 判断高亮是否为本页
         wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#mainsrp-pager > div > div > div > ul > li.item.active > span'),str(page_number)))
+        get_products()
     except TimeoutError:
         # 如果出现TimeoutError的错误,则继续
         next_page(page_number)
+
+# 解析内容
+def get_products():
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-itemlist .items .item')))
+    html = driver.page_source  # 获得网页源码
+    doc = pq(html)
+    items = doc('#mainsrp-itemlist .items .item').items()  # 得到所有的items
+    for item in items:
+        product = {
+            'image': item.find('.pic .img').attr('src'),
+            'price': item.find('.price').text(),
+            'deal': item.find('.deal-cnt').text()[:-3],
+            'title': item.find('.title').text(),
+            'shop': item.find('.shop').text(),
+            'location': item.find('.location').text()
+        }
+        print(product)
+
 
 def main():
     total = search()
